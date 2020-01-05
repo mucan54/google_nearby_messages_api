290af13a4f6d811816e877834297bbd026f8707a
mucan54 <mucan54@hotmail.com>
2020-01-05 15:58:30 +0300

    Update GoogleNearbyMessagesApiPlugin.m

--- ios/Classes/GoogleNearbyMessagesApiPlugin.m
+++ ios/Classes/GoogleNearbyMessagesApiPlugin.m
@@ -2,9 +2,6 @@
 #import <google_nearby_messages_api/google_nearby_messages_api-Swift.h>
 #import <GNSMessages.h>
 
-GNSMessageManager *messageManager =
-    [[GNSMessageManager alloc] initWithAPIKey:kMyAPIKey];
-
 @implementation GoogleNearbyMessagesApiPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftGoogleNearbyMessagesApiPlugin registerWithRegistrar:registrar];
